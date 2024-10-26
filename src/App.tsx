d45a95236145391134619967b5bc2fedd557abda
Bárbara Dias <barbara.dias30@outlook.com>
2024-10-25 22:52:43 -0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 import TargetList from "./components/TargetList";
 import TargetForm from "./components/TargetForm";
-import TodoForm from "./components/TodoForm";
-import styles from "./styles/App.module.css";
 
 const App: React.FC = () => {
   const [refresh, setRefresh] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [selectedTargetId, setSelectedTargetId] = useState<number | null>(null);
 
+  // Função para atualizar a lista de Targets e gerar um novo valor de key
   const refreshTargets = () => setRefresh(!refresh);
 
   return (
-    <div className={styles.appContainer}>
+    <div>
       <TargetForm onSuccess={refreshTargets} />
-      {selectedTargetId && (
-        <TodoForm targetId={selectedTargetId} onSuccess={refreshTargets} />
-      )}
-      <TargetList key={refresh.toString()} />
+      {/* Gera um timestamp como key para garantir que seja único */}
+      <TargetList key={refresh ? Date.now() : Date.now() + 1} />
     </div>
   );
 };
