4d63d780139e08c21f33b4a9c38a8216bcd34b1d
Bárbara Dias <barbara.dias30@outlook.com>
2024-10-25 22:26:09 -0300

    Detalhes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import TargetList from "./components/TargetList";
 import TargetForm from "./components/TargetForm";
-import styles from "./App.module.css";
+import TodoForm from "./components/TodoForm";
+import styles from "./styles/App.module.css";
 
 const App: React.FC = () => {
   const [refresh, setRefresh] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [selectedTargetId, setSelectedTargetId] = useState<number | null>(null);
 
   const refreshTargets = () => setRefresh(!refresh);
 
   return (
     <div className={styles.appContainer}>
       <TargetForm onSuccess={refreshTargets} />
+      {selectedTargetId && (
+        <TodoForm targetId={selectedTargetId} onSuccess={refreshTargets} />
+      )}
       <TargetList key={refresh.toString()} />
     </div>
   );
